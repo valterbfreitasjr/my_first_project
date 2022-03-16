98081d56117f00fbf6a83f528632b0d92d312be8
Valter B. Freitas Jr <valter191@hotmail.com>
2022-03-15 22:13:36 -0300

    form-criado!

--- src/js/source.js
+++ src/js/source.js
@@ -1,5 +1,9 @@
 function alertPage(phrase) {
     var name = document.getElementById("nome").value;
-    
+    var email = document.getElementById("email").value;
+    var contato = document.getElementById("contato").value;
+    console.log(name);
+    console.log(email);
+    console.log(contato);
     alert(phrase + ' ' + name);
 }
